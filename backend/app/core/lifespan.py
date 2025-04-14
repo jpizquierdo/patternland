3ed3331a2c6f51f66f0e21f2ea75bbf8f0cca6c3
Joel Pérez Izquierdo <joelperez91@gmail.com>
2025-04-14 13:21:30 +0100

    add S3 boto3 basic logic

--- backend/app/core/lifespan.py
+++ backend/app/core/lifespan.py
@@ -1,13 +1,17 @@
 from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from sqlmodel import Session
+
 from app.core.database import create_db_and_tables, engine
+from app.core.s3_storage import create_bucket
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):  # pragma: no cover
+async def lifespan(app: FastAPI):  # pragma: no cover # noqa: ARG001
     # Startup events
     with Session(engine) as session:
         create_db_and_tables(session)
+    create_bucket()
     yield
     # Shutdown events
