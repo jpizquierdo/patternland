66898a25b0740420a649bfd3766a2e92eb8cb986
Joel Pérez Izquierdo <joelperez91@gmail.com>
2025-04-21 15:09:20 +0100

    Add permission checks for pattern actions in frontend and backend as #5

--- frontend/src/components/Common/PatternActionsMenu.tsx
+++ frontend/src/components/Common/PatternActionsMenu.tsx
@@ -1,7 +1,7 @@
 import { IconButton } from "@chakra-ui/react"
 import { BsThreeDotsVertical } from "react-icons/bs"
 import { MenuContent, MenuRoot, MenuTrigger } from "../ui/menu"
-
+import { useQueryClient } from "@tanstack/react-query"
 import type { PatternPublic } from "@/client"
 import DeletePattern from "../Patterns/DeletePattern"
 import EditPattern from "../Patterns/EditPattern"
@@ -9,12 +9,16 @@ import AddFiles from "../Patterns/AddFiles"
 import PatternFilesView from "../Patterns/PatternFilesView"
 import { useState } from "react"
 
+import type { UserPublic } from "@/client"
 interface PatternActionsMenuProps {
   pattern: PatternPublic
 }
 
 export const PatternActionsMenu = ({ pattern }: PatternActionsMenuProps) => {
   const [open, setOpen] = useState(false);
+  const queryClient = useQueryClient()
+  // Get the current user from the query cache
+  const currentUser = queryClient.getQueryData<UserPublic>(["currentUser"])
   return (
     <MenuRoot open={open} onOpenChange={({ open }) => setOpen(open)}>
       <MenuTrigger asChild>
@@ -23,10 +27,19 @@ export const PatternActionsMenu = ({ pattern }: PatternActionsMenuProps) => {
         </IconButton>
       </MenuTrigger>
       <MenuContent>
-        <AddFiles id={pattern.id} closeMenu={() => setOpen(false)} />
+        {/* Conditionally render AddFiles if the user has permissions */}
+        {(currentUser?.is_superuser || currentUser?.id == pattern.owner_id) && (
+          <AddFiles id={pattern.id} closeMenu={() => setOpen(false)} />
+        )}
         <PatternFilesView pattern={pattern} closeMenu={() => setOpen(false)} />
-        <EditPattern pattern={pattern} closeMenu={() => setOpen(false)} />
-        <DeletePattern id={pattern.id} />
+        {/* Conditionally render EditPattern if the user has permissions */}
+        {(currentUser?.is_superuser || currentUser?.id == pattern.owner_id) && (
+          <EditPattern pattern={pattern} closeMenu={() => setOpen(false)} />
+        )}
+        {/* Conditionally render DeletePattern if the user has permissions */}
+        {(currentUser?.is_superuser || currentUser?.id == pattern.owner_id) && (
+          <DeletePattern id={pattern.id} />
+        )}
       </MenuContent>
     </MenuRoot>
   )
