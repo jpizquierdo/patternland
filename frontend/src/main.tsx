9bb6e231c9d49e982333e133fd75d716ffb55204
Joel Pérez Izquierdo <joelperez91@gmail.com>
2025-04-27 08:29:27 +0100

    Docker image to docker hub (#16)
    * Implement Docker image build and push workflow; update frontend to use dynamic API URL from config.json
    
    * Remove initial development warning from README
    
    * Add TAG-latest variable to my.env.example for Docker image tagging

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,25 +12,11 @@ import { routeTree } from "./routeTree.gen"
 import { ApiError, OpenAPI } from "./client"
 import { CustomProvider } from "./components/ui/provider"
 
-OpenAPI.BASE = import.meta.env.VITE_API_URL
-OpenAPI.TOKEN = async () => {
-  return localStorage.getItem("access_token") || ""
+async function loadConfig() {
+  const response = await fetch("/config.json");
+  const config = await response.json();
+  return config;
 }
-
-const handleApiError = (error: Error) => {
-  if (error instanceof ApiError && [401, 403].includes(error.status)) {
-    localStorage.removeItem("access_token")
-    window.location.href = "/login"
-  }
-}
-const queryClient = new QueryClient({
-  queryCache: new QueryCache({
-    onError: handleApiError,
-  }),
-  mutationCache: new MutationCache({
-    onError: handleApiError,
-  }),
-})
 
 const router = createRouter({ routeTree })
 declare module "@tanstack/react-router" {
@@ -39,12 +25,34 @@ declare module "@tanstack/react-router" {
   }
 }
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <CustomProvider>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
-    </CustomProvider>
-  </StrictMode>,
-)
+loadConfig().then((config) => {
+  OpenAPI.BASE = config.VITE_API_URL;
+  OpenAPI.TOKEN = async () => {
+    return localStorage.getItem("access_token") || ""
+  }
+
+  const handleApiError = (error: Error) => {
+    if (error instanceof ApiError && [401, 403].includes(error.status)) {
+      localStorage.removeItem("access_token")
+      window.location.href = "/login"
+    }
+  }
+  const queryClient = new QueryClient({
+    queryCache: new QueryCache({
+      onError: handleApiError,
+    }),
+    mutationCache: new MutationCache({
+      onError: handleApiError,
+    }),
+  })
+
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <CustomProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </CustomProvider>
+    </StrictMode>,
+  );
+});
